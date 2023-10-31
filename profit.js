4909c5da840bdb5caeb26b37da7ae3d284b258c3
TanzilaMomin <147149600+TanzilaMomin@users.noreply.github.com>
2023-10-31 22:07:56 +0530

    first commit

---
+++ profit.js
@@ -0,0 +1,7 @@
+document.write("<h1>Profit Calculator</h1>")
+
+var cp = prompt("Enter the cost price: ")
+var sp = prompt("Enter the selling price:")
+var profit = sp - cp;
+document.write("Total Profit")
+document.write("<b>" + profit + "</b>")
